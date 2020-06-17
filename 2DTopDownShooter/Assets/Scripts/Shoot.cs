6ad8cdc4fa2b9b9dbdef1999f4d846265fdb2e87
Jonas Fuhlendorff <jonas.fuhlendorff@bitzerdk.com>
2020-06-17 10:01:11 +0200

    Simple backup

---
+++ 2DTopDownShooter/Assets/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {       
+            GameObject newBullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
+            Rigidbody2D rigidbody = newBullet.GetComponent<Rigidbody2D>();
+            rigidbody.velocity = newBullet.transform.up * 23;
+            
+            Destroy(newBullet, 10);
+        }
+    }
+}
+
+
